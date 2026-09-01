71d4af689b1677f9fbc11133d80cb05339ab263a
Sara Tariq <saratariq222@gmail.com>
2026-09-01 20:44:06 +0300

    Add files via upload

---
+++ 04_Projects/17. Data Cleaning Project.sql
@@ -0,0 +1,49 @@
+-- Data Cleaning 
+
+SELECT *
+FROM layoffs;
+
+
+-- 1. Remove Duplicates 
+-- 2. Standardize the Data
+-- 3. NULL values or blank values 
+-- 4. Remove Any Columns
+
+
+
+CREATE TABLE layoffs_staging
+LIKE layoffs;
+
+SELECT *
+FROM layoffs_staging;
+
+INSERT layoffs_staging
+SELECT *
+FROM layoffs;
+
+
+
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, industry, total_laid_off, percentage_laid_off, `date`) AS row_num
+FROM layoffs_staging;
+
+
+WITH duplicate_cte AS 
+(
+SELECT *,
+ROW_NUMBER() OVER(
+PARTITION BY company, location, 
+industry, total_laid_off, percentage_laid_off, `date`, stage,
+country, funds_raised_millions) AS row_num
+FROM layoffs_staging
+)
+SELECT *
+FROM duplicate_cte 
+WHERE row_num > 1;
+
+
+SELECT *
+FROM layoffs_staging
+WHERE company = 'casper';
+
